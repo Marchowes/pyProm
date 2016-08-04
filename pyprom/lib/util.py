a3b8cb3241e221c5c326f3e9713f6b58d42e2597
Marc Howes <marc.h.howes@gmail.com>
2016-08-03 22:19:20 -0400

    + Arcsec span calculator.

--- pyprom/lib/util.py
+++ pyprom/lib/util.py
@@ -1,9 +1,20 @@
 from __future__ import division
+import math
 
 def dottedDecimaltoDegrees(coordinate):
     minute, second = divmod(coordinate*3600,60)
     degree, minute = divmod(minute, 60)
     return degree, minute, second
 
-def degreesToDottedDecimal(deg, mnt, sec):
-    return float("%.5f" % (deg + (mnt / 60) + (sec / 3600)))
+def degreesToDottedDecimal(deg, mnt=0, sec=0):
+	"""
+	Accepts dms and converts to dd
+	"""
+    return float("{0:.5f}".format (deg + (mnt / 60) + (sec / 3600)))
+
+def longitudeArcSec(longitude):
+	"""
+	Accepts longitude in dotted decimal notation, and returns 
+	Arcsecond distance in meters.
+	"""
+	return math.cos(math.radians(longitude))*30.87
