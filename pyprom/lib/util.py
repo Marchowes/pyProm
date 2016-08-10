d80df4e3b11455f267e502d58a59f28765b561b7
Marc Howes <marc.h.howes@gmail.com>
2016-08-10 19:38:17 -0400

    Bug Fix in Util.

--- pyprom/lib/util.py
+++ pyprom/lib/util.py
@@ -7,14 +7,14 @@ def dottedDecimaltoDegrees(coordinate):
     return degree, minute, second
 
 def degreesToDottedDecimal(deg, mnt=0, sec=0):
-	"""
-	Accepts dms and converts to dd
-	"""
-    return float("{0:.5f}".format (deg + (mnt / 60) + (sec / 3600)))
+    """
+    Accepts dms and converts to dd
+    """
+    return float("{0:.5f}".format(deg + (mnt / 60) + (sec / 3600)))
 
 def longitudeArcSec(longitude):
-	"""
-	Accepts longitude in dotted decimal notation, and returns 
-	Arcsecond distance in meters.
-	"""
-	return math.cos(math.radians(longitude))*30.87
+    """
+    Accepts longitude in dotted decimal notation, and returns 
+    Arcsecond distance in meters.
+    """
+    return math.cos(math.radians(longitude))*30.87
