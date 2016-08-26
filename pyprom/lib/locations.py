d86438583e5211d2829528bff4641b113a677ef8
Marc Howes <marc.h.howes@gmail.com>
2016-08-25 20:34:15 -0400

    - location objects now have special object attributes - findShores()

--- pyprom/lib/locations.py
+++ pyprom/lib/locations.py
@@ -1,6 +1,7 @@
 """
 This lib contains objects for storing various geographic data.
 """
+from collections import defaultdict
 
 
 class BaseCoordinate(object):
@@ -11,6 +12,23 @@ class BaseCoordinate(object):
     def __init__(self, latitude, longitude, *args, **kwargs):
         self.latitude = latitude
         self.longitude = longitude
+
+    def __eq__(self, other):
+        return [self.latitude, self.longitude] ==\
+               [other.latitude, other.longitude]
+
+    def __ne__(self, other):
+        return [self.latitude, self.longitude] !=\
+               [other.latitude, other.longitude]
+
+    def __hash__(self):
+        return hash((self.latitude, self.longitude))
+
+    def __repr__(self):
+        return "<BaseCoordinate> lat {} long {}".format(self.latitude,
+                                                        self.longitude)
+
+    __unicode__ = __str__ = __repr__
 
 
 class SpotElevation(BaseCoordinate):
@@ -26,6 +44,24 @@ class SpotElevation(BaseCoordinate):
     def feet(self):
         return self.elevation * 3.2808
 
+    def __eq__(self, other):
+        return [self.latitude, self.longitude, self.elevation] ==\
+               [other.latitude, other.longitude, other.elevation]
+
+    def __ne__(self, other):
+        return [self.latitude, self.longitude, self.elevation] != \
+               [other.latitude, other.longitude, other.elevation]
+
+    def __hash__(self):
+        return hash((self.latitude, self.longitude, self.elevation))
+
+    def __repr__(self):
+        return "<SpotElevation> lat {} long {} El {}".format(self.latitude,
+                                                             self.longitude,
+                                                             self.feet)
+
+    __unicode__ = __str__ = __repr__
+
 
 class Summit(SpotElevation):
     """
@@ -36,10 +72,12 @@ class Summit(SpotElevation):
                                      elevation, *args, **kwargs)
         self.multiPoint = kwargs.get('multiPoint', None)
 
-    def __str__(self):
-        return "Summit El {} lat {} long {}".format(self.feet,
-                                                    self.latitude,
-                                                    self.longitude)
+    def __repr__(self):
+        return "<Summit> lat {} long {} El {}".format(self.feet,
+                                                      self.latitude,
+                                                      self.longitude)
+
+    __unicode__ = __str__ = __repr__
 
 
 class SpotElevationContainer(object):
@@ -81,6 +119,11 @@ class SpotElevationContainer(object):
         return [x for x in self.points if x.elevation > lower and
                 x.elevation < upper]
 
+    def __repr__(self):
+        return "<SpotElevationContainer> {} Objects".format(len(self.points))
+
+    __unicode__ = __str__ = __repr__
+
 
 class BaseGridPoint(object):
     def __init__(self, x, y):
@@ -91,6 +134,14 @@ class BaseGridPoint(object):
         """
         self.x = x
         self.y = y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __repr__(self):
+        return "<BaseGridPoint> x: {}, y: {}".format(self.x, self.y)
+
+    __unicode__ = __str__ = __repr__
 
 
 class GridPoint(BaseGridPoint):
@@ -103,6 +154,22 @@ class GridPoint(BaseGridPoint):
         """
         super(GridPoint, self).__init__(x, y)
         self.elevation = elevation
+
+    def __eq__(self, other):
+        return [self.x, self.y, self.elevation] ==\
+               [other.x, other.y, other.elevation]
+
+    def __ne__(self, other):
+        return [self.x, self.y, self.elevation] !=\
+               [other.x, other.y, other.elevation]
+
+    def __repr__(self):
+        return "<GridPoint> x: {}, y: {}, elevation(m); {}".\
+               format(self.x,
+                      self.y,
+                      self.elevation)
+
+    __unicode__ = __str__ = __repr__
 
 
 class MultiPoint(object):
@@ -125,16 +192,86 @@ class MultiPoint(object):
         for gridpoint in self.points:
             neighbors = self.analyzeData.iterateDiagonal(gridpoint.x,
                                                          gridpoint.y)
-            edgeList = list()
+            nonEqualNeighborList = list()
+            equalNeighborList = list()
             for _x, _y, elevation in neighbors:
                 if elevation != self.elevation:
-                    edgeList.append(GridPoint(_x, _y, elevation))
-            if edgeList:
+                    nonEqualNeighborList.append(GridPoint(_x, _y, elevation))
+                elif elevation == self.elevation:
+                    equalNeighborList.append(GridPoint(_x, _y, elevation))
+
+            if nonEqualNeighborList:
                 edgeObjectList.append(EdgePoint(gridpoint.x,
                                                 gridpoint.y,
                                                 self.elevation,
-                                                edgeList))
+                                                nonEqualNeighborList,
+                                                equalNeighborList))
         return edgeObjectList
+
+    def findShores(self):
+        """
+        Function will find all shores along pond-like blob. and add all
+        discontigous shore points as lists within the returned list.
+        This is needed for finding Islands.
+        :return: List of lists of `GridPoint` representing a Shore
+        """
+        edge = self.findEdge()
+        # Flatten list and find unique members.
+        shorePoints = list(set([val for sublist in
+                           [x.nonEqualNeighbors for x in edge]
+                           for val in sublist]))
+
+        # For Optimized Lookups on larger lists.
+        shoreIndex = defaultdict(list)
+        for shorePoint in shorePoints:
+            shoreIndex[shorePoint.x].append(shorePoint.y)
+        purgedIndex = defaultdict(list)
+
+        # initialize the shoreList and its index.
+        shoreList = list()
+        shoreList.append(list())
+        listIndex = 0
+
+        # Grab some shorePoint to start with.
+        masterGridPoint = shorePoints[0]
+        toBeAnalyzed = [masterGridPoint]
+
+        # toBeAnalyzed preloaded with the first point in the shorePoints list.
+        # First Act is to pop a point from toBeAnalyzed, and analyze it's
+        # orthogonal neighbors for shorePoint members and add them to the
+        # toBeAnalyzed list. These points are also added to a purgedIndex,
+        # as well as dropped from shoreIndex.
+        # Once neighbors are depleted, a new point is pulled from the
+        # shorePoints list the list index is incremented and its neighbors are
+        # analyzed until shorePoints is depleted.
+
+        while True:
+            if not toBeAnalyzed:
+                if shorePoints:
+                    listIndex += 1
+                    shoreList.append(list())
+                    toBeAnalyzed = [shorePoints[0]]
+            if not toBeAnalyzed:
+                break
+            try:
+                gridPoint = toBeAnalyzed.pop()
+            except:
+                return shoreList
+
+            if gridPoint.y not in purgedIndex[gridPoint.x]:
+                shorePoints.remove(gridPoint)
+                shoreIndex[gridPoint.x].remove(gridPoint.y)
+                purgedIndex[gridPoint.x].append(gridPoint.y)
+                shoreList[listIndex].append(gridPoint)
+            else:
+                continue
+            neighbors = self.analyzeData.iterateOrthogonal(gridPoint.x,
+                                                           gridPoint.y)
+            for _x, _y, elevation in neighbors:
+                candidate = GridPoint(_x, _y, elevation)
+                if candidate.y in shoreIndex[candidate.x]:
+                    toBeAnalyzed.append(candidate)
+        return shoreList
 
     @property
     def pointsLatLong(self):
@@ -146,6 +283,13 @@ class MultiPoint(object):
                 self.analyzeData.datamap.y_position_longitude(coord.y))
                 for coord in self.points]
 
+    def __repr__(self):
+        return "<Multipoint> elevation(m): {}, points {}". \
+            format(self.elevation,
+                   len(self.points))
+
+    __unicode__ = __str__ = __repr__
+
 
 class EdgePoint(GridPoint):
     """
@@ -156,7 +300,18 @@ class EdgePoint(GridPoint):
     :param elevation: elevation in meters
     :param nonEqualNeighbors: list of GridPoints that are non equal in height
            in comparison to the EdgePoint.
+    :param equalNeighbors: list of GridPoints that are equal in height
+           in comparison to the EdgePoint.
     """
-    def __init__(self, x, y, elevation, nonEqualNeighbors):
+    def __init__(self, x, y, elevation, nonEqualNeighbors, equalNeighbors):
         super(EdgePoint, self).__init__(x, y, elevation)
         self.nonEqualNeighbors = nonEqualNeighbors
+        self.equalNeighbors = equalNeighbors
+
+    def __repr__(self):
+        return "<EdgePoint> ele(m): {}, #Eq Points {}, #NonEq Points {}".\
+            format(self.elevation,
+                   len(self.equalNeighbors),
+                   len(self.nonEqualNeighbors))
+
+    __unicode__ = __str__ = __repr__
