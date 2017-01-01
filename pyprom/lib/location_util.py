f276e43da0bf4f682282956e0d38cdfbe5d2da37
Marc Howes <marc.h.howes@gmail.com>
2017-01-01 11:19:45 -0500

    Deprecate Island.

--- pyprom/lib/location_util.py
+++ pyprom/lib/location_util.py
@@ -9,31 +9,6 @@ We need this utility to be seperate becasue of circular dependencies.
 import math
 
 
-def findExtremities(points):
-    """
-    :param points: list of GridPoint type objects
-    :return: Dict of N,S,E,W extremities
-    """
-    N = S = E = W = list([points[0]])
-    for point in points:
-        if point.x < N[0].x:
-            N = [point]
-        elif point.x == N[0].x:
-            N.append(point)
-        if point.x > S[0].x:
-            S = [point]
-        elif point.x == S[0].x:
-            S.append(point)
-        if point.y > E[0].y:
-            E = [point]
-        elif point.y == E[0].y:
-            E.append(point)
-        if point.y < W[0].y:
-            W = [point]
-        elif point.y == W[0].y:
-            W.append(point)
-    return {'N': N, 'S': S, 'E': E, 'W': W}
-
 def longitudeArcSec(longitude):
     """
     Accepts longitude in dotted decimal notation, and returns
