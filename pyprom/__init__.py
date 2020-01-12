94a7c13f60fc86b4c35061f798649c019e885864
Marc <marc.h.howes@gmail.com>
2020-01-12 00:04:10 -0500

    bump version

--- pyprom/__init__.py
+++ pyprom/__init__.py
@@ -6,7 +6,7 @@ the LICENSE file that accompanies it.
 
 PyProm: This library includes tools for surface network analysis.
 """
-version_info = (0, 6, 4)
+version_info = (0, 7, 0)
 __name__ = 'pyProm'
 __doc__ = 'A python surface network analysis script'
 __author__ = 'Marc Howes'
