56f13072ab6b692c9dcc024daa65514d2b822370
Marc Howes <marc.h.howes@gmail.com>
2025-12-14 18:59:25 -0500

    define constants.

--- pyprom/lib/constants.py
+++ pyprom/lib/constants.py
@@ -11,8 +11,13 @@ DOMAIN_EXTENSION = ".dom"
 # Unit Conversion Constants
 
 METERS_TO_FEET = 0.3048
+METERS_TO_SURVEY_FOOT = 0.3048006096
 
 FEET_TO_METERS = 1/METERS_TO_FEET
+SURVEY_FOOT_TO_METERS = 1/METERS_TO_FEET
 
 FEET_IN_MILES = 5280
 
+GDAL_METERS = ('m', 'metre', 'meter',)
+GDAL_FEET = ('ft', 'foot', 'feet',)
+GDAL_SURVEY_FOOT = ('us survey foot', 'US survey foot', 'us-ft',)
