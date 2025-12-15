33d74e56f9236be60e5ddb5df9654fb4364a9de8
Marc Howes <marc.h.howes@gmail.com>
2025-12-14 21:34:29 -0500

    Add common Type Hints.

---
+++ pyprom/_typing/type_hints.py
@@ -0,0 +1,7 @@
+from typing import NewType
+
+NUMPY_X = NewType("NUMPY_X", int)
+NUMPY_Y = NewType("NUMPY_Y", int)
+
+LATITUDE_Y = NewType("LATITUDE_Y", float)
+LONGITUDE_X = NewType("LONGITUDE_X", float)
