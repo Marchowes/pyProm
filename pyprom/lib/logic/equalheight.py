313d1893bfbb05db060485dda05cc1731826804b
Marc Howes <marc.h.howes@gmail.com>
2025-12-26 19:35:43 -0500

    Typehints and set.

--- pyprom/lib/logic/equalheight.py
+++ pyprom/lib/logic/equalheight.py
@@ -10,8 +10,22 @@ from collections import defaultdict
 from ..containers.multipoint import MultiPoint
 from ..containers.perimeter import Perimeter
 
+from typing import TYPE_CHECKING, List, Set
+if TYPE_CHECKING:
+    from pyprom.lib.datamaps import DataMap
+    from pyprom._typing.type_hints import (
+        Numpy_X, Numpy_Y, 
+        Elevation, 
+        XY, 
+        XY_Elevation
+    )
 
-def equalHeightBlob(datamap, x, y, elevation):
+
+def equalHeightBlob(
+        datamap: DataMap, 
+        x: Numpy_X, y: Numpy_Y, 
+        elevation: Elevation
+    ):
     """
     This function generates a
     :class:`pyprom.lib.containers.multipoint.MultiPoint`
@@ -25,16 +39,16 @@ def equalHeightBlob(datamap, x, y, elevation):
     :return: MultiPoint Object containing all x,y coordinates and elevation
     :rtype: :class:`pyprom.lib.containers.multipoint.MultiPoint`
     """
-    masterGridPoint = (x, y, elevation)
-    exploredEqualHeight = defaultdict(dict)
-    memberPoint = list()
+    masterGridPoint: XY_Elevation = (x, y, elevation)
+    exploredEqualHeight: Set[XY] = set()
+    memberPoint: List[XY] = list()
     _add_member_point(x, y, exploredEqualHeight, memberPoint)
 
     perimeterPointHash = defaultdict(dict)
     perimeterPoints = list()
-    toBeAnalyzed = [masterGridPoint]
-    perimeterMapEdge = set()
-    multipointEdges = []
+    toBeAnalyzed: List[XY_Elevation] = [masterGridPoint]
+    perimeterMapEdge: Set[XY_Elevation] = set()
+    multipointEdges: List[XY_Elevation] = []
 
     if datamap.is_map_edge(x, y):
         multipointEdges.append((x, y, elevation))
@@ -51,8 +65,7 @@ def equalHeightBlob(datamap, x, y, elevation):
         for _x, _y, elevation in neighbors:
             if elevation is None or elevation == datamap.nodata:
                 continue
-            elif elevation == masterGridPoint[2] and\
-                    not exploredEqualHeight[_x].get(_y, False):
+            elif elevation == masterGridPoint[2] and not (_x,_y) in exploredEqualHeight:
                 branch = (_x, _y, elevation)
                 _add_member_point(_x, _y, exploredEqualHeight, memberPoint)
                 if datamap.is_map_edge(_x, _y):
@@ -68,28 +81,32 @@ def equalHeightBlob(datamap, x, y, elevation):
                         _add_perimeter_point(_x, _y, (_x, _y, elevation), perimeterPointHash, perimeterPoints)
                     if datamap.is_map_edge(_x, _y):
                         perimeterMapEdge.add((_x, _y, elevation))
-    return MultiPoint(memberPoint,
-                      masterGridPoint[2],
-                      datamap,
-                      perimeter=Perimeter(
-                          pointList=perimeterPoints,
-                          pointIndex=perimeterPointHash,
-                          datamap=datamap,
-                          mapEdge=edge,
-                          mapEdgePoints=list(perimeterMapEdge))),\
+    return (
+        MultiPoint(
+            memberPoint,
+            masterGridPoint[2],
+            datamap,
+            perimeter=Perimeter(
+                pointList=perimeterPoints,
+                pointIndex=perimeterPointHash,
+                datamap=datamap,
+                mapEdge=edge,
+                mapEdgePoints=list(perimeterMapEdge))
+            ),
         multipointEdges
+    )
 
-def _add_member_point(x, y, hash, list):
+def _add_member_point(
+        x: Numpy_X, y: Numpy_X, 
+        explored_set: Set[XY], 
+        list: List[XY]
+    ) -> None:
     """
     Adds point to list and to hash
-    :param x: x coordinate
-    :param y: y coordinate
-    :param hash: hash to add to
-    :param list: list to add to
-    :return:
     """
-    hash[x][y] = True
-    list.append((x, y))
+    xy = (x,y)
+    explored_set.add(xy)
+    list.append(xy)
 
 def _add_perimeter_point(x, y, val, hash, list):
     """
